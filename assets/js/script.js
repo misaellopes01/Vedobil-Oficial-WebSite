4fcc1facc7b664f2dd6904aabc37b4cfb8a76217
misaellopes01 <misael.lopes01@hotmail.com>
2021-01-17 06:40:57 +0100

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,14 +35,22 @@ sr.reveal('.about-left', {});
 sr.reveal('.about-right', { delay: 400 });
 sr.reveal('.about__text', { delay: 400 });
 
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle', {});
-sr.reveal('.skills__text', {});
-sr.reveal('.skills__data', { interval: 200 });
-sr.reveal('.skills__img', { delay: 600 });
+/*SCROLL SERVICES*/
+sr.reveal('.card', {});
+sr.reveal('', {});
+sr.reveal('.btn2', { interval: 200 });
+sr.reveal('', { delay: 600 });
 
 /*SCROLL WORK*/
 sr.reveal('.work__img', { interval: 200 });
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input', { interval: 200 });
+//====================================================================
+//==== SHOW SOLICITAR SERVIÇO ===
+$("#abrir").click(function() {
+    $(".container").toggle().css("display", "flex");
+});
+$("#btn-3").click(function() {
+    $(".container").toggle().css("display", "none");
+});
